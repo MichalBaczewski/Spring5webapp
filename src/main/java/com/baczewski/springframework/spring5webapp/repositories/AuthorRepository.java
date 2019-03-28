7d69164b95f3dbeea8fdd3ce9fa33121ece081b4
MichalBaczewski <michal.baczewski@outlook.com>
2019-03-28 19:37:23 +0100

    Adding JPA example.

---
+++ src/main/java/com/baczewski/springframework/spring5webapp/repositories/AuthorRepository.java
@@ -0,0 +1,7 @@
+package com.baczewski.springframework.spring5webapp.repositories;
+
+import com.baczewski.springframework.spring5webapp.model.Author;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AuthorRepository extends CrudRepository <Author, Long> {
+}
